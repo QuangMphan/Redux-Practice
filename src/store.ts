bf9c0da94eaf37b36e2d9f6e4b194dec293ab2bc
QuangMphann <quangphan2705@gmail.com>
2024-06-03 17:51:55 +0700

    createReducer and createAction

---
+++ src/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import blogReducer from './pages/blog/blog.reducer'
+
+//  Get the root state and dispatch types from the store
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
+export const store = configureStore({
+    reducer: {
+        blog: blogReducer
+    }
+})
